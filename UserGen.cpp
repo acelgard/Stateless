a831bb969bc2833d9666252dc6d1aadf6e5c7a01
acelgard <aceelgard@comcast.net>
2020-11-11 19:47:26 -0600

    Basic for loop and if statment (knocking the dust off)

--- UserGen.cpp
+++ UserGen.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!";
+    cout << "Hello World!\n";
+    int k = 3;
+    int a = 5;
+    for (int i = 0; i < 7; i++){
+        cout << "my number is " << k;
+        k++;
+        cout << " my second number is " << a << endl;
+        a++;
+        cout << "i = " << i;
+        if (i == 6){
+            cout << "\nthe end!";
+        }
+
+
+
+    }
     return 0;
 }
 //testing
