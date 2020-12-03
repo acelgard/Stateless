69f36ead8e5266d9c34cfa3b3d6f1b21d8c028d6
acelgard <aceelgard@comcast.net>
2020-12-02 18:49:31 -0600

    random token made, names might need to come from a list

--- UserGen.cpp
+++ UserGen.cpp
@@ -1,25 +1,64 @@
-//test
+//Username creation Dec 2 2020
+#include <chrono>
 #include <iostream>
+#include <vector>
+#include <iomanip>
+#include <stdlib.h>
 //#include
 using namespace std;
 
+
+
+
+uint32_t getTick() {
+    struct timespec ts;
+    unsigned theTick = 0U;
+    clock_gettime( CLOCK_REALTIME, &ts );
+    theTick  = ts.tv_nsec / 1000000;
+    theTick += ts.tv_sec * 1000;
+    return theTick;
+}
+
+string randomName(int length) {
+
+  char consonents[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
+  char vowels[] = {'a','e','i','o','u','y'};
+
+  string name = "";
+
+  int random = rand() % 2;
+  int count = 0;
+
+  for(int i = 0; i < length; i++) {
+
+    if(random < 2 && count < 2) {
+      name = name + consonents[rand() % 19];
+      count++;
+    }
+    else {
+      name = name + vowels[rand() % 5];
+      count = 0;
+    }
+
+    random = rand() % 2;
+
+  }
+
+  return name;
+
+}
+
 int main() {
+    srand(getTick());
+    int LENGTH = 25;
     cout << "Hello World!\n";
+    cout << randomName(LENGTH) << endl;
     int k = 3;
     int a = 5;
-    for (int i = 0; i < 7; i++){
-        cout << "my number is " << k;
-        k++;
-        cout << " my second number is " << a << endl;
-        a++;
-        cout << "i = " << i;
-        if (i == 6){
-            cout << "\nthe end!";
-        }
-
-
-
+    for (int i = 0; i < 99; i++){
+       cout << randomName(LENGTH) << endl;
     }
+    cout << "\nthe end!";
     return 0;
 }
 //testing
