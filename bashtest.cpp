0d0a75cd3db6c281011fc000b9362f78933cb0e8
acelgard <aceelgard@comcast.net>
2020-12-06 16:01:01 -0600

    updated encryption through bash

--- bashtest.cpp
+++ bashtest.cpp
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 
 // ....
-
+using namespace std;
 int main(){
+    int index = 2;
 system("./echotest.sh");
 system("echo \"this is a test\"");
-system("echo \" \\\ this is a test\"");
+system("cat tokens/gen_test.txt");
+string tester = "this a test" + to_string(index) + "rest";
 
-
+string encryptTK = "gpg -r Cryptoproject --encrypt tokens/gen_test" + to_string(index) + ".txt";
+system(encryptTK.c_str());
 }
