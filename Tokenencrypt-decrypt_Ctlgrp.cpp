90c02071345575076fd7a1c6866b4cd23564645a
acelgard <aceelgard@comcast.net>
2020-12-07 00:50:57 -0600

    Final commit

--- Tokenencrypt-decrypt_Ctlgrp.cpp
+++ Tokenencrypt-decrypt_Ctlgrp.cpp
@@ -6,25 +6,31 @@
 
 using namespace std;
 int main(){
-    int index = 2;
-    int tkTTL = 3;
+    int i = 0;
+    const int TTL = 3;
     
 //
-for (int i=0; i<1790; i++ ){
-    for (int TTL=3; TTL!=0; TTL-- ){
-        /*
-        string encryptTK = "gpg -r Cryptoproject --encrypt tokens/token" + to_string(i) + to_string(TTL) + ".txt";
-        system(encryptTK.c_str());
-*/
-        cout << "outer i = " << i << "\tinner for (TTL) = " << TTL << endl;
-        
-    }
+while (i<1790)
+{
+  
+    //Client has signed in with propper credentials and needs a token
+    string encryptTK = "gpg -r Cryptoproject --encrypt tokens/token" + to_string(i) + to_string(TTL) + ".txt";
+    system(encryptTK.c_str());
+    for (int j=0;j<10;j++){
+        //After client sends a request for a new page. The server, being simulated by this program, will decrypt the token and verify it (15 secure pages simulated)
+      
 
+        string decryptTK = ("gpg --passphrase Test --decrypt tokens/token" + to_string(i) + to_string(TTL) + ".txt.gpg");
+        system(decryptTK.c_str());
+    }
+    //after the conection is dropped the server deletes the old token
+    string purge = "rm tokens/token" + to_string(i) + to_string(TTL) + ".txt.gpg";
+    system(purge.c_str());
+
+    //increase the counter to the next user
+    i += 5;
 }
 
-
-string purge = "rm tokens/token" + to_string(index) + to_string(tkTTL) + ".txt.pgp";
-system(purge.c_str());
 }
 
 //----helpful debug commands
